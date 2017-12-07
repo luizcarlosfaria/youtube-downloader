0df49aab0a08cd02e85469598f70583db809e000
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2017-12-07 08:33:50 -0200

    Adição da dependência do minio Adição do youtube-dl e phython na imagem base do projeto worker Começo do fluxo completo de download, upload para o minio e exclusão do arquivo temporário

--- src/DevWeekWorker/Program.cs
+++ src/DevWeekWorker/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using Minio;
 
 namespace DevWeekWorker
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var outputFileName = $"/shared/{Guid.NewGuid().ToString("N")}.mp4";
+            string videoUrl = "https://www.youtube.com/watch?v=qfNmyxV2Ncw";
+
+            await DownloadVideo(videoUrl, outputFileName);
+            await UploadToS3("videos", outputFileName);
+            DropLocalOutputFile(outputFileName);
+
+
             Console.WriteLine("Hello World!");
+        }
+
+        
+
+        public static async Task DownloadVideo(string videoUrl, string outputFileName)
+        {
+            var process = System.Diagnostics.Process.Start(new ProcessStartInfo("youtube-dl", $"-o {outputFileName} {videoUrl}")
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            });
+            process.WaitForExit();
+            if (process.ExitCode != 0)
+            {
+                Console.WriteLine(await process.StandardError.ReadToEndAsync());
+                Console.WriteLine(await process.StandardOutput.ReadToEndAsync());
+                throw new ApplicationException("Download Failure");
+            }
+            string[] files = System.IO.Directory.GetFiles("/shared/", "*", SearchOption.AllDirectories);
+            Console.WriteLine(string.Join(Environment.NewLine, files));
+        }
+
+        public static async Task UploadToS3(string bucketName, string fileName)
+        {
+            MinioClient minio = new MinioClient(
+                "s3:9000",
+                "Zu8VgBoZMU2xcmOEeS70",
+                "xvnjYyFQyFs44iuUagi4kTHiOGvlK1PiX64LiwOy"
+                );
+
+            bool exists = await minio.BucketExistsAsync(bucketName);
+            if (exists == false)
+            {
+                await minio.MakeBucketAsync(bucketName);
+            }
+            await minio.PutObjectAsync(bucketName, System.IO.Path.GetFileName(fileName), fileName);
+        }
+
+        private static void DropLocalOutputFile(string outputFileName)
+        {
+            System.IO.File.Delete(outputFileName);
         }
     }
 }
