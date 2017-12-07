9ed9857d626e901d39f21bbaba77f1189bf40e41
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2017-12-06 22:30:32 -0200

    default projects - no changes

---
+++ src/DevWeekWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevWeekWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
