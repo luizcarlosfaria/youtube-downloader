8631014e026b1babe50818e6a73e2b0bda33e2e4
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2017-12-24 10:56:53 -0200

    Executando o rebuild cache a cada 2 minutos (manualmente)

--- src/DevWeek.Worker/Program.cs
+++ src/DevWeek.Worker/Program.cs
@@ -29,6 +29,15 @@ namespace DevWeek
                 appContext = ContextBuilder.BuildContext();
             });
 
+            Task.Run(async () => {
+                while(true)
+                {
+                    await appContext.GetObject<Services.DataService>().RebuildCache();
+                    System.Threading.Thread.Sleep(TimeSpan.FromMinutes(2));
+                }
+                
+            });
+
 
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             Task.Run(async () =>
